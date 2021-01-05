7b0c1285d58a32d4d6b0b3b39af2660bcab7d515
novel-yaguchi <takuya.yaguchi@novel.jpn.com>
2021-01-05 21:24:43 +0900

    add favorite modal

--- src/views/pages/Home.tsx
+++ src/views/pages/Home.tsx
@@ -19,9 +19,10 @@ const customStyles = {
     left: '50%',
     right: 'auto',
     bottom: 'auto',
+    padding: '40px',
     marginRight: '-50%',
-    height: '70%',
-    width: '70%',
+    height: '80%',
+    width: '64%',
     transform: 'translate(-50%, -50%)',
   },
   overlay: {
@@ -53,6 +54,44 @@ function Home() {
         <div className="home-favorite--day">{cat.date.getFullYear()}/{cat.date.getMonth()}/{cat.date.getDate()} {cat.date.getHours()}:{cat.date.getMinutes()}</div>
       </div >
     );
+  }
+  
+
+  function FavoriteList(props: { catInfo: any[]; openModal: any; }) {
+    const favoriteCatList = !props.catInfo
+      ? []
+      : props.catInfo.map((cat: any) => <FavoriteItem cat={cat} openModal={props.openModal}/>)
+
+    return (
+      <div className="home-favorite--list">
+        {favoriteCatList}
+      </div>
+    )
+  }
+
+  function FavoriteItem(props: { cat: any; openModal: ((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void) | undefined; }) {
+    const cat = props.cat;
+
+    const formattedDate = formatDate(cat.date);
+
+    return (
+      <div className="home-favorite--icon" onClick={props.openModal}>
+        <img className="home-favorite--img" src={cat.image} />
+        <div className="home-favorite--day">	
+          {formattedDate}
+        </div>
+      </div >
+    )
+  }
+
+  function formatDate(date: { getFullYear: () => any; getMonth: () => any; getDate: () => { (): any; new(): any; }; getHours: () => any; getMinutes: () => any; }) {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate()();
+    const hours = date.getHours();
+    const minutes =date.getMinutes();
+
+    return `${year}/${month}/${day} ${hours}:${minutes}`
   }
 
   useEffect(() => {
@@ -89,7 +128,7 @@ function Home() {
   return (
     <div className="home-main">
       <div>
-        <Modal
+        {/* <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
           style={customStyles}
@@ -109,8 +148,8 @@ function Home() {
               </div>
             </div> 
           </div>
-        </Modal>
-        {/* <Modal
+        </Modal> */}
+        <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
           style={customStyles}
@@ -122,18 +161,28 @@ function Home() {
           <div className="home-modal--img">
             <img className="home-modal--cat" src={recommendationImage} />
           </div>
-          <div>
-            お気に入り登録日
-          </div>
-          <div className="home-modal--button" onClick={saveFavorite}>
-            <div className="home-modal--icon">
-              <div className="home-modal--favoritebutton">
-                <img className="home-modal--favoriteimg" src={star} />
-                <div className="home-modal--text" >お気に入りからはずす</div>
+          <div className="home-modal--item">
+            
+            <div className="home-modal--favorite">
+              <div className="home-modal--favorite--text">
+                お気に入り登録日
               </div>
-            </div> 
+              <div className="home-modal--favorite--date">
+                2020/11/25 15:42
+                {/* <div className="home-favorite--day">{cat.date.getFullYear()}/{cat.date.getMonth()}/{cat.date.getDate()} {cat.date.getHours()}:{cat.date.getMinutes()}</div> */}
+              </div>
+            </div>
+
+            <div className="home-modal--button" onClick={saveFavorite}>
+              <div className="home-modal--icon">
+                <div className="home-modal--removebutton">
+                  <img className="home-modal--favoriteimg" src={star} />
+                  <div className="home-modal--text" >お気に入りからはずす</div>
+                </div>
+              </div> 
+            </div>
           </div>
-        </Modal> */}
+        </Modal>
       </div>
 
       <div className="home-content">
@@ -155,6 +204,9 @@ function Home() {
           <div className="home-favorite--title">
             あなたのお気に入り
           </div>
+          {/* <FavoriteList
+            catInfo={catInfo}
+            openModal={openModal}/> */}
           <div className="home-favorite--list">
             { listItems }
           </div>
